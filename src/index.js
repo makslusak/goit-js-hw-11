0d532351af9526ce2cb55a554a04b5f8e064f986
makslusak <106471649+makslusak@users.noreply.github.com>
2022-09-23 19:15:29 +0300

    Final proj

--- src/index.js
+++ src/index.js
@@ -3,36 +3,73 @@ import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { formRef, galleryRef } from './js/refs/index';
+import { formRef, galleryRef, moreBtnRef, lessBtnRef } from './js/refs/index';
 import { getData } from './js/api/index';
 import { createMarkup } from './js/markup/index';
+let pageNumber = 1;
+let valueFromInput = '';
+let gallery = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
 
-formRef.addEventListener('submit', onFormClick);
+formRef.addEventListener('submit', onSubmit);
+moreBtnRef.addEventListener('click', loadMore);
+lessBtnRef.addEventListener('click', loadLess);
 
-async function onFormClick(evt) {
+async function onSubmit(evt) {
   evt.preventDefault();
+  pageNumber = 1;
 
-  const valueFromInput = evt.target.searchQuery.value;
+  valueFromInput = evt.target.searchQuery.value.trim();
+  if (!valueFromInput) {
+    Notify.warning('Enter data to search');
+    return;
+  }
 
-  try {
-    const { data } = await getData(valueFromInput, 1);
-    validData(data);
-    console.log(data);
-    const markup = createMarkup(data, valueFromInput);
+  takeData();
+}
+async function loadMore(evt) {
+  pageNumber += 1;
 
-    galleryRef.innerHTML = markup;
-  } catch (error) {}
+  takeData();
+}
+async function loadLess(evt) {
+  pageNumber -= 1;
+  takeData();
 }
 
 function validData(data) {
   if (data.total === 0) {
+    invisibleBtn();
     return Notify.failure(
-      "We're sorry, but you've reached the end of search results."
+      'Sorry, there are no images matching your search query. Please try again.'
     );
   }
   Notify.success(`Hooray! We found ${data.total} images.`);
+  visibleBtn();
 }
-let gallery = new SimpleLightbox('.gallery a', {
-  captionDelay: 250,
-});
+
+function visibleBtn() {
+  moreBtnRef.classList.remove('is-hidden');
+  lessBtnRef.classList.remove('is-hidden');
+}
+function invisibleBtn() {
+  moreBtnRef.classList.add('is-hidden');
+  lessBtnRef.classList.add('is-hidden');
+}
+
 gallery.on('show.simplelightbox');
+
+async function takeData() {
+  try {
+    const { data } = await getData(valueFromInput, pageNumber);
+    validData(data);
+    console.log(data);
+    const markup = createMarkup(data, valueFromInput);
+    galleryRef.innerHTML = markup;
+    gallery.refresh();
+    window.scrollTo(0, 0);
+  } catch (error) {
+    console.log(error);
+  }
+}
--- src/js/refs/index.js
+++ src/js/refs/index.js
@@ -1,2 +1,4 @@
 export const formRef = document.querySelector('#search-form');
 export const galleryRef = document.querySelector('.gallery');
+export const moreBtnRef = document.querySelector('.load-more');
+export const lessBtnRef = document.querySelector('.load-less');
